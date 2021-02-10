b1885c766a978c73ad09f8b7f84808769febb338
SLAYER-USERBOT <78257750+SLAYER-USERBOT@users.noreply.github.com>
2021-02-10 20:41:13 +0530

    Update __main__.py

--- slayerbot/__main__.py
+++ slayerbot/__main__.py
@@ -1,4 +1,4 @@
-#    Copyright (C) Midhun KM 2020-2021
+#    Copyright (C) SLAYER 2020-2021
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
@@ -20,13 +20,13 @@ from telethon import TelegramClient
 from telethon import __version__ as tv
 import sys
 import platform
-from fridaybot import bot, client2, client3, friday_version
-from fridaybot.Configs import Config
+from slayerbot import bot, client2, client3, slayer_version
+from slayerbot.Configs import Config
 from telethon.tl.types import InputMessagesFilterDocument
-from fridaybot.utils import load_module, start_assistant, load_module_dclient
-from fridaybot.Configs import Config
+from slayerbot.utils import load_module, start_assistant, load_module_dclient
+from slayerbot.Configs import Config
 
-fridaydevs = logging.getLogger("Friday")
+slayerdevs = logging.getLogger("Slayer")
 
 async def add_bot(bot_token):
     await bot.start(bot_token)
@@ -38,7 +38,7 @@ async def add_bot(bot_token):
 async def check_inline_on_warner(ws):
     w_s = await ws.get_me()
     if not w_s.bot_inline_placeholder:
-        fridaydevs.info("Warning : We Have Detected That You Have Not Turned On Inline Mode For Your Assistant Bot, Please Go To @BotFather And Enable This.")
+        slayerdevs.info("Warning : We Have Detected That You Have Not Turned On Inline Mode For Your Assistant Bot, Please Go To @BotFather And Enable This.")
     return
 Lol = "folyl's Token"
 async def lol_s(client):
@@ -47,30 +47,30 @@ async def lol_s(client):
     
 def multiple_client():
     if client2:
-        fridaydevs.info("Starting Client 2")
+        slayerdevs.info("Starting Client 2")
         try:
             warnerstark = None
             client2.start()
             client2.loop.run_until_complete(lol_s(client2))
         except:
             warnerstark = True
-            fridaydevs.info("Client 2 Failed To Load. Check Your String.")
+            slayerdevs.info("Client 2 Failed To Load. Check Your String.")
     if client3:
-        fridaydevs.info("Starting Client 3")
+        slayerdevs.info("Starting Client 3")
         try:
             chsaiujwal = None
             cleint3.start
             client3.loop.run_until_complete(lol_s(client3))
         except:
             chsaiujwal = True
-            fridaydevs.info("Client 3 Failed To Load.")
+            slayerdevs.info("Client 3 Failed To Load.")
     if not client2:
         warnerstark = True
     if not client3:
         chsaiujwal = True
     return warnerstark, chsaiujwal    
 
-async def get_other_plugins(Config, client_s, fridaydevs):
+async def get_other_plugins(Config, client_s, slayerdevs):
     try:
         a_plugins = await client_s.get_messages(
             entity=Config.LOAD_OTHER_PLUGINS_CHNNL,
@@ -79,16 +79,16 @@ async def get_other_plugins(Config, client_s, fridaydevs):
             search=".py",
         )
     except:
-        fridaydevs.info("Failed To Other Modules :(")
+        slayerdevs.info("Failed To Other Modules :(")
         return
     for meisnub in a_plugins:
         hmm = meisnub.media.document.attributes[-1].file_name
-        pathh = "fridaybot/modules/"
+        pathh = "slayerbot/modules/"
         if os.path.exists(os.path.join(pathh, hmm)):
             pass
         else:
-            await client_s.download_media(meisnub.media, "fridaybot/modules/")
-    fridaydevs.info("Extra Plugins Downloaded.")
+            await client_s.download_media(meisnub.media, "slayerbot/modules/")
+    slayerdevs.info("Extra Plugins Downloaded.")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
@@ -109,7 +109,7 @@ if Config.LOAD_OTHER_PLUGINS:
         
 import glob
 
-path = "fridaybot/modules/*.py"
+path = "slayerbot/modules/*.py"
 files = glob.glob(path)
 failed_warner = 0
 for name in files:
@@ -120,9 +120,9 @@ for name in files:
             load_module(shortname.replace(".py", ""))    
         except Exception as e:
             failed_warner += 1
-            fridaydevs.info("------------------------")
-            fridaydevs.info("Failed To Load : " + str(shortname.replace(".py", "")) + f" Error : {str(e)}")
-            fridaydevs.info("------------------------")
+            slayerdevs.info("------------------------")
+            slayerdevs.info("Failed To Load : " + str(shortname.replace(".py", "")) + f" Error : {str(e)}")
+            slayerdevs.info("------------------------")
         if failed2 is None:
             try:
                 load_module_dclient(shortname.replace(".py", ""), client2)
@@ -135,16 +135,16 @@ for name in files:
                 pass
 
 if Config.ENABLE_ASSISTANTBOT == "ENABLE":
-    path = "fridaybot/modules/assistant/*.py"
+    path = "slayerbot/modules/assistant/*.py"
     files = glob.glob(path)
     for name in files:
         with open(name) as f:
             path1 = Path(f.name)
             shortname = path1.stem
             start_assistant(shortname.replace(".py", ""))
-    wsta = "Friday And Assistant Bot Have Been Installed Successfully !"
+    wsta = "Slayer And Assistant Bot Have Been Installed Successfully !"
 else:
-    wsta = "Friday Has Been Installed Sucessfully"
+    wsta = "Slayer Has Been Installed Sucessfully"
 
 total_clients = 1
 if failed2 is None:
@@ -158,11 +158,11 @@ else:
    exit()
 fridaydevs.info(f"""{wsta}
 -------------------------------------------
-Friday-Userbot Based On Telethon V{tv}
+Slayer-Userbot Based On Telethon V{tv}
 Python Version : {platform.python_version()}
 Friday-Userbot Version : V{friday_version}
-Support Chat : @FridayChat
-Updates Channel : @FridaySupportOfficial
+Support Chat : @Slayer_userbot_Support
+Updates Channel : @Slayer_Userbot
 Total Clients : {total_clients}
 -------------------------------------------""")
         
