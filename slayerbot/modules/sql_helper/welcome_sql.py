b3db6634225160034cb2ac4bbd329ddd61d19041
SLAYER-USERBOT <78257750+SLAYER-USERBOT@users.noreply.github.com>
2021-02-10 17:42:49 +0530

    Update welcome_sql.py

--- slayerbot/modules/sql_helper/welcome_sql.py
+++ slayerbot/modules/sql_helper/welcome_sql.py
@@ -1,6 +1,6 @@
 from sqlalchemy import BigInteger, Boolean, Column, String, UnicodeText
 
-from fridaybot.modules.sql_helper import BASE, SESSION
+from slayerbot.modules.sql_helper import BASE, SESSION
 
 
 class Welcome(BASE):
