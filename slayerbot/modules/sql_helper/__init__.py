4102d74daf83ff631398d570edb908f2539dcc6b
SLAYER-USERBOT <78257750+SLAYER-USERBOT@users.noreply.github.com>
2021-02-10 18:05:56 +0530

    Update __init__.py

--- slayerbot/modules/sql_helper/__init__.py
+++ slayerbot/modules/sql_helper/__init__.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 # the secret configuration specific things
-from fridaybot.Configs import Config
+from slayerbot.Configs import Config
 
 
 def start() -> scoped_session:
