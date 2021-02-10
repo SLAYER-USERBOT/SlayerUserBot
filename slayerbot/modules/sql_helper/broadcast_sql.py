b2ec03b48c29e3564988c6e06749a21a4dd836a3
SLAYER-USERBOT <78257750+SLAYER-USERBOT@users.noreply.github.com>
2021-02-10 18:10:00 +0530

    Update broadcast_sql.py

--- slayerbot/modules/sql_helper/broadcast_sql.py
+++ slayerbot/modules/sql_helper/broadcast_sql.py
@@ -1,4 +1,4 @@
-#    Copyright (C) 2020-2021 @HeisenbergTheDanger
+#    Copyright (C) 2020-2021 ֆʟǟʏɛʀ υѕєявσт ™
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
