beed536ad47664d05de6a3bebde5d4e5bb396ef4
SLAYER-USERBOT <78257750+SLAYER-USERBOT@users.noreply.github.com>
2021-02-11 11:35:08 +0530

    Update bot_alive.py

--- slayerbot/modules/assistant/bot_alive.py
+++ slayerbot/modules/assistant/bot_alive.py
@@ -11,17 +11,17 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from fridaybot.Configs import Config
+from slayerbot.Configs import Config
 import time
 from telethon import __version__ as tv
 import sys
 import platform
 from git import Repo
-from fridaybot import ALIVE_NAME
-from fridaybot.modules import currentversion
+from slayerbot import ALIVE_NAME
+from slayerbot.modules import currentversion
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_IMG = "https://telegra.ph/file/22535f8051a58af113586.jpg"
+PM_IMG = "https://telegra.ph/file/60569be50266114d5e44e.jpg"
 pm_caption = "➥ **ASSISTANT IS:** `ONLINE`\n\n"
 pm_caption += "➥ **SYSTEMS STATS**\n"
 pm_caption += f"➥ **Telethon Version:** `{tv}` \n"
@@ -31,12 +31,12 @@ pm_caption += "➥ **Current Branch** : `master`\n"
 pm_caption += f"➥ **Version** : `{currentversion}`\n"
 pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n"
 pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`\n\n"
-pm_caption += "➥ **License** : [GNU General Public License v3.0](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n"
-pm_caption += "➥ **Copyright** : By [StarkGang@Github](GitHub.com/StarkGang)\n"
-pm_caption += "[Assistant By Friday 🇮🇳](https://telegra.ph/FRIDAY-06-15)"
+pm_caption += "➥ **License** : [GNU General Public License v3.0](github.com/SLAYER-USERBOT/SlayerUserbot/blob/master/LICENSE)\n"
+pm_caption += "➥ **Copyright** : By [OWNER](https://t.me/Slayer_UserBot)\n"
+pm_caption += "[Assistant By Slayer 🇮🇳](https://t.me/Slayer_UserBot)"
 
 # only Owner Can Use it
 @assistant_cmd("alive", is_args=False)
 @peru_only
-async def friday(event):
+async def slayer(event):
     await tgbot.send_file(event.chat_id, PM_IMG, caption=pm_caption)
